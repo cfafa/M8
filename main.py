6006290122f1fa0a46d969b79b91167784d79ea3
cfafa <143934336+cfafa@users.noreply.github.com>
2024-04-05 23:15:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template,request, redirect
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/list')
+def list():
+    return render_template('list_organisations.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
