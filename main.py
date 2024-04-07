03b52482c40c2bee443edf69d1754e94f4881537
cfafa <143934336+cfafa@users.noreply.github.com>
2024-04-07 19:04:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/list')
-def list():
+@app.route('/lists')
+def lists():
     return render_template('list_organisations.html')
+
+@app.route('/guides')
+def guides():
+    return render_template('eco_guides.html')
+
+@app.route('/centers')
+def centers():
+    return render_template('recycling centers.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
